7270abd60cd2454c624825b258106fe9b08ecf00
silvio usai <berzylius@hotmail.it>
2021-04-28 18:04:47 +0200

    fine es base

--- js/script.js
+++ js/script.js
@@ -10,13 +10,19 @@ var app = new Vue(
     {
         el: '#root',
         data: {
-
+            cds: []
         },
         methods: {
 
         },
         mounted() {
-
+            axios
+            .get('https://flynn.boolean.careers/exercises/api/array/music')
+            .then((response) => {
+                const result = response.data;
+                console.log(response.data);
+                this.cds = result.response;
+            });
         }
     }
 );
