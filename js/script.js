db84a36ce46608141e5a132d5dc3da35027da52f
silvio usai <berzylius@hotmail.it>
2021-04-28 17:43:14 +0200

    template base

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// Attraverso una chiamata ajax all’API di boolean https://flynn.boolean.careers/exercises/api/array/music 
+// avremo a disposizione una decina di dischi musicali. 
+// Utilizzando vue, stampiamo a schermo una card per ogni album.
+
+// BONUS: Creare una select con tutti i generi dei dischi. In base a cosa scegliamo nella select, 
+// vedremo i corrispondenti cd.
+// BONUS 2: Ordinare i dischi per anno di uscita.
+
+var app = new Vue(
+    {
+        el: '#root',
+        data: {
+
+        },
+        methods: {
+
+        },
+        mounted() {
+
+        }
+    }
+);
